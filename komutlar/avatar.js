2374f5ed3b47b9053178979a48d311cf152c7308
SadeceErXan <79266201+SadeceErXan@users.noreply.github.com>
2021-02-18 22:02:19 +0300

    Add files via upload

---
+++ komutlar/avatar.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js')
+
+exports.run = async (client, message, args) => {
+  let avatar = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=2048`//avatar komutu nasıl bulamadım bende böyle bişey yaptım v12 için yanlız bu :d
+let Embed = new MessageEmbed()
+
+            Embed.setTitle(`İşte İstediğin Avatar!`)
+            Embed.setThumbnail(avatar)
+            Embed.setColor(`RANDOM`)
+            Embed.setDescription(`**-HYDRA**`)
+            return message.channel.send(Embed)
+        
+    }
+
+
+exports.conf = {
+    enabled:false,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0,
+}
+
+exports.help = {
+    name: 'avatar',
+    description: 'avatarınızı mesaj olarak atar',
+    usage: '(prefix)avatar '
+}
